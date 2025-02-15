3f71db84cbefa01d1b14897699a34536f107ce13
GlitchiPitch <nikitareutsky@gmail.com>
2025-02-15 15:45:07 +0500

    change utility place

--- src/Shared/Utility/clearConnections.lua
+++ src/Shared/Utility/clearConnections.lua
@@ -2,6 +2,7 @@ local function clearConnections(_connections: { RBXScriptConnection })
     for _, connect in _connections do
         connect:Disconnect()
     end
+    _connections = {}
 end
 
 return clearConnections
